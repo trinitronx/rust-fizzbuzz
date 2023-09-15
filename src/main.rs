5afa53560281c25ab918b0aeee609291f3de42c8
James Cuzella <james.cuzella@lyraphase.com>
2023-09-14 18:27:37 -0600

    Minify solution for Code Golf scoring

--- src/main.rs
+++ src/main.rs
@@ -29,23 +29,7 @@ which allows us to write hideously minified code.
 Running `rustfmt` on this suffers the whitespace penalty, but does make it more
 readable.
 */
-fn main() {
-    for i in 1..101 {
-        let f = i % 3 == 0;
-        let b = i % 5 == 0;
-        let fb = f && b;
-        if f {
-            print!("Fizz");
-        }
-        if b {
-            print!("Buzz");
-        }
-        if !fb && !f && !b {
-            print!("{i}");
-        }
-        println!();
-    }
-}
+fn main() {for i in 1..101{let f=i%3==0;let b=i%5==0;let fb=f&&b;if f{print!("Fizz");}if b{print!("Buzz");}if !fb&&!f&&!b{print!("{i}");}println!();}}
 
 // Another way in 192 chars
 // fn d(a:i32,b:i32)->bool{a&b==0}fn fizz_buzz(){for i in 1..100{let mut s=format!("");if d(i,3){s=format!("Fizz");}if d(i,5){s+="Buzz";}println!("{}",if s.is_empty(){format!("{i}")} else{s});}}
