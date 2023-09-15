8a818bd2e97f87671aedbb02f87b1abf665b69d3
James Cuzella <james.cuzella@lyraphase.com>
2023-09-15 04:14:59 -0600

    Move crate-level docs into README.md

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,4 @@
-/*! # FizzBuzz Code Golf
-
-Consider the following problem:
-
-Write a short program that prints each number from 1 to 100 on a new line.
- - For each multiple of 3, print "`Fizz`" instead of the number.
- - For each multiple of 5, print "`Buzz`" instead of the number.
- - For numbers which are multiples of both 3 and 5, print "`FizzBuzz`" instead of the number.
- - Write a solution (or reduce an existing one) so it has as few characters as possible.
-
-Scoring
-- Your score is: `200 - number of characters in your source code) / 100`
-
-Source: [HackerRank: FizzBuzz][1]
-
-[1]: https://www.hackerrank.com/challenges/fizzbuzz/problem
-*/
+#![doc = include_str!("../README.md")]
 
 /**
 # Rust `FizzBuzz` in 125 chars
