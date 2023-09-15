45b60adc39937e29404add221ed5eea8c8e2ffff
trinitronx <trinitronx@users.noreply.github.com>
2023-09-15 09:10:07 +0000

    deploy: 4adea435983ea0c55bec1966ca52daed9145bbae

--- docs/search-index.js
+++ docs/search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"fizzbuzz":{"doc":"FizzBuzz Code Golf","t":"F","n":["main"],"q":[[0,"fizzbuzz"]],"d":["Rust <code>FizzBuzz</code> in 151 chars"],"i":[0],"f":[[[]]],"c":[],"p":[]}\
+"fizzbuzz":{"doc":"FizzBuzz Code Golf","t":"OFF","n":["l","main","pain"],"q":[[0,"fizzbuzz"]],"d":["","Rust <code>FizzBuzz</code> in 125 chars",""],"i":[0,0,0],"f":[0,[[]],[[]]],"c":[],"p":[]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
